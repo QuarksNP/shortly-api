5fd4ef071b67bec015e8653ee12cc983dd5cd4d3
Alberto Guzman <moly1682@gmail.com>
2023-04-24 17:14:51 -0400

    fixing description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,14 +1,17 @@
-import GetStarted from "../common/btnGetStarted/btnGetStarted";
+import Image from "next/image"
+import ilustration from "../../../public/images/illustration-working.svg"
+import GetStarted from "../common/btnGetStarted/btnGetStarted"
 import styles from "./Description.module.css"
 
 export default function Description() {
     return (
         <section className={styles.section}>
+            <Image src={ilustration} width={600} height={600} alt="working-ilustration"/>
             <div className={styles.article}>
                 <article>
                     <h1 className={styles.title}>More than just shorter links</h1>
                     <p className={styles.description}>
-                        Build your brand's recognition and get detailed insight on how your
+                        Build your brand is recognition and get detailed insight on how your
                             links are performing.
                     </p>
                 </article>
