7c85d1d610d5abe98b6321375f6c38bff7bb3220
Alberto Guzman <moly1682@gmail.com>
2023-04-24 17:04:39 -0400

    test deployment description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,12 +1,9 @@
-import Image from "next/image"
-import ilustration from "../../../public/images/illustration-working.svg";
 import GetStarted from "../common/btnGetStarted/btnGetStarted";
 import styles from "./Description.module.css"
 
 export default function Description() {
     return (
         <section className={styles.section}>
-            <Image src={ilustration} width={600} height={600}/>
             <div className={styles.article}>
                 <article>
                     <h1 className={styles.title}>More than just shorter links</h1>
